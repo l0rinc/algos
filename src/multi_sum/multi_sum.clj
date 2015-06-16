ea0d9fa1e9f1fbc7ae01b282fee11f610ab58be0
Pap Lőrinc <paplorinc@yahoo.com>
2015-06-16 23:12:13 +0300

    Added squared, linearithmic and linear solutions for the double-sum problem

--- src/multi_sum/multi_sum.clj
+++ src/multi_sum/multi_sum.clj
@@ -1,5 +1,5 @@
 (ns multi_sum.multi_sum
-  (:import (java.util Arrays))
+  (:import (java.util Collections))
   (:use multi_sum.rand_range))
 
 ; Given N distinct integers, how many triples sum to exactly zero?
@@ -9,12 +9,47 @@
 (def elems-range 1000)
 (assert (<= elems-count elems-range))
 
-; ------------------------------------------------------------------- ;
+; ------------------------------------------------------- ;
 
 (def elems (distinct-rand-ints elems-count elems-range))
 (assert (= elems-count (count elems)))
 
-(defn triple-sum-cubic [elems]
+(if debug (println (seq elems)))
+
+(defn double-sum-squared [elems]
+  (for [i' (range 0 elems-count)
+        j' (range (inc i') elems-count)
+        :let [i (nth elems i')
+              j (nth elems j')]
+        :when (zero? (+ i j))]
+    #{i j}))
+(println ["double-sum-squared"                              ; TODO get name of method from reference
+          (count (double-sum-squared elems))                ; TODO extract print to method
+          (double-sum-squared elems)])                      ; TODO print time also and run it only once
+
+
+(defn double-sum-linearithmic [elems]
+  (let [sorted-elems (sort elems)]
+    (for [i' (range 0 elems-count)
+          :let [i (nth elems i')]
+          :when (<= i' (Collections/binarySearch sorted-elems (- i)))] ; TODO custom Clojure binary search?
+      #{i (- i)})))
+(println ["double-sum-linearithmic"
+          (count (double-sum-linearithmic elems))
+          (double-sum-linearithmic elems)])
+
+
+(defn double-sum-linear [elems]                             ; TODO the set should be generated on the fly, to avoid duplicates and 0s
+  (let [existing (set elems)]                               ; TODO unify with linearithmic?
+    (for [i elems
+          :when (contains? existing (- i))]
+      #{i (- i)})))
+(println ["double-sum-linear"
+          (count (double-sum-linear elems))
+          (double-sum-linear elems)])
+
+
+(defn triple-sum-cubic [elems]                              ; TODO with binary search? With hashing?
   (for [i' (range 0 elems-count)
         j' (range (inc i') elems-count)
         k' (range (inc j') elems-count)
@@ -23,9 +58,6 @@
               k (nth elems k')]
         :when (zero? (+ i j k))]
     #{i j k}))
-
-(println (count (triple-sum-cubic elems)))
-
-(if debug
-  [(println (Arrays/toString elems))
-   (time (triple-sum-cubic elems))])
+(println ["triple-sum-cubic"
+          (count (triple-sum-cubic elems))
+          (triple-sum-cubic elems)])
