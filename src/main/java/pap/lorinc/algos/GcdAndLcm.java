206014294429d5afd337a0598a75abcc1c2fe0f2
Pap Lőrinc <paplorinc@yahoo.com>
2015-10-25 20:15:46 +0200

    Added tests (example- and property based) for GCD and LCM

--- src/main/java/pap/lorinc/algos/GcdAndLcm.java
+++ src/main/java/pap/lorinc/algos/GcdAndLcm.java
@@ -1,46 +1,16 @@
 package pap.lorinc.algos;
 
-import java.util.Random;
-
-import static java.lang.String.format;
-import static java.lang.System.out;
-import static pap.lorinc.algos.helpers.Numbers.assertDivides;
+import static java.lang.Math.multiplyExact;
 
 public class GcdAndLcm {
-    private static final Random RAND = new Random();
-    private static final int MAGNITUDE = 100;
-
-    public static void main(String... args) {
-        int first = RAND.nextInt(MAGNITUDE), second = RAND.nextInt(MAGNITUDE);
-
-        printGcd(first, second);
-        printLcm(first, second);
-    }
-
-    private static void printGcd(int first, int second) {
-        int gcd = gcd(first, second);
-        assertDivides(first, gcd);
-        assertDivides(second, gcd);
-        out.println(format("Gcd of %s and %s is %s", first, second, gcd));
-    }
-
-    private static void printLcm(int first, int second) {
-        int lcm = lcm(first, second);
-        assertDivides(lcm, first);
-        assertDivides(lcm, second);
-        if (lcm > first * second) throw new IllegalStateException();
-        out.println(format("Lcm of %s and %s is %s", first, second, lcm));
-    }
-
-
     /* TODO @tailrec or iterative */
-    private static int gcd(int first, int second) {
-        int remainder = first % second;
+    public static long gcd(long first, long second) {
+        long remainder = first % second;
         return (remainder == 0) ? second
                                 : gcd(second, remainder);
     }
 
-    private static int lcm(int first, int second) {
-        return first / gcd(first, second) * second;
+    public static long lcm(long first, long second) throws ArithmeticException {
+        return multiplyExact(first / gcd(first, second), second);
     }
 }
---
+++ src/test/java/pap/lorinc/algos/GcdAndLcmTest.groovy
@@ -0,0 +1,58 @@
+package pap.lorinc.algos
+
+import spock.genesis.Gen
+import spock.lang.Specification
+
+import static pap.lorinc.algos.GcdAndLcm.gcd
+import static pap.lorinc.algos.GcdAndLcm.lcm
+import static pap.lorinc.algos.helpers.Numbers.canDivide
+
+class GcdAndLcmTest extends Specification {
+    private static int ITERATIONS = 1000
+    private static IntRange RANGE = 0..Integer.MAX_VALUE
+
+    /*@formatter:off*/
+    def 'Greatest common divisor - by example'(long first, long second, long result) {
+        when:   def gcd = gcd(first, second);
+        then:   gcd == result
+        where:  first | second || result
+                2     | 4      || 2
+                9     | 28     || 1
+                42    | 56     || 14
+                48    | 180    || 12
+                54    | 24     || 6
+    }
+
+    def 'Greatest common divisor - property based'(long first, long second) {
+        when:   def gcd = gcd(first, second);
+
+        then:   canDivide(first, gcd);
+        and:    canDivide(second, gcd);
+
+        where:  first << Gen.integer(RANGE) * ITERATIONS
+                second << Gen.integer(RANGE) * ITERATIONS
+    }
+
+    def 'Least common multiple by example'(long first, long second, long result) {
+        when:   def lcm = lcm(first, second);
+        then:   lcm == result
+        where:  first | second || result
+                2     | 4      || 4
+                3     | 4      || 12
+                24    | 300    || 600
+                48    | 180    || 720
+                65    | 330    || 4290
+    }
+
+    def 'Least common multiple by example - property based'(long first, long second) {
+        when:   def lcm = lcm(first, second);
+
+        then:   canDivide(lcm, first);
+                canDivide(lcm, second);
+                lcm <= first * second
+
+        where:  first << Gen.integer(RANGE) * ITERATIONS
+                second << Gen.integer(RANGE) * ITERATIONS
+    }
+    /*@formatter:on*/
+}
