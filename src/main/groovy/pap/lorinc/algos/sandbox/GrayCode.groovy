840134cff4ed55d2f2b9a2b6aac852933995512f
Pap Lőrinc <paplorinc@yahoo.com>
2015-12-01 21:07:05 +0200

    Simplified the Binary to Gray converter :p

--- src/main/groovy/pap/lorinc/algos/sandbox/GrayCode.groovy
+++ src/main/groovy/pap/lorinc/algos/sandbox/GrayCode.groovy
@@ -1,13 +1,7 @@
 package pap.lorinc.algos.sandbox
 
 class GrayCode {
-    static fromBinary(BigInteger num) {
-        def bits = num.toString(2).collect { it as int }
-
-        def gray = [bits[0]]
-        for (int i = 0; i < bits.size() - 1; i++)
-            gray << (bits[i] ^ bits[i + 1])
-
-        new BigInteger(gray.join(''), 2).intValue()
+    static fromBinary(long num) {
+        num ^ (num >> 1)
     }
 }
