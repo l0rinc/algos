a8376bec5887d036704404815826d26eb0c38e6f
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-09 19:04:08 +0200

    a^3 + b^3 = c^3 + d^3

---
+++ src/main/groovy/pap/lorinc/algos/sandbox/CubeSumEquality.groovy
@@ -0,0 +1,37 @@
+package pap.lorinc.algos.sandbox
+
+/* for a,b,c,d in 1..1000 find all, where a**3 + b**3 = c**3 + d**3 */
+class CubeSumEquality {
+    static final RANGE = 1..1000
+
+    static void main(String... args) {
+        def sumMap = collectSumPairEntries()
+
+        def count = 0
+        iterate(sumMap.values()) { a, b, c, d -> count += 1 }
+        println count
+    }
+
+    static collectSumPairEntries() {
+        def sumMap = [:]
+        def cubes = RANGE.collectEntries { [(it): it**3] }
+        cubes.each { a, a3 ->
+            cubes.each { b, b3 ->
+                def sum = a3 + b3
+                sumMap[sum] = sumMap.getOrDefault(sum, []) + [[a, b]]
+            }
+        }
+        sumMap
+    }
+
+    static iterate(Collection groups, Closure closure) {
+        groups.each { pairs ->
+            pairs.each { a, b ->
+                pairs.each { c, d ->
+                    closure(a, b, c, d)
+                }
+            }
+        }
+    }
+}
+
