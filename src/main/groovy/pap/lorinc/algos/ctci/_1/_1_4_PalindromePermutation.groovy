43effe8464937f23f0097302279c8f2937e6e346
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-15 18:06:21 +0200

    CTCI 1.4: PalindromePermutation

---
+++ src/main/groovy/pap/lorinc/algos/ctci/_1/_1_4_PalindromePermutation.groovy
@@ -0,0 +1,12 @@
+package pap.lorinc.algos.ctci._1
+
+/** Given a string, check whether palindromes can be formed from it */
+class _1_4_PalindromePermutation {
+    /** Complexity: O(string.size()) */
+    static canBePalindrome(String string) {
+        def normalizedString = string.toLowerCase().replaceAll('\\W', '')
+        def groups = normalizedString.collect().groupBy()
+        def oddGroups = groups.findAll { k, v -> (v.size() % 2 != 0) }
+        oddGroups.size() <= 1
+    }
+}
---
+++ src/test/groovy/pap/lorinc/algos/ctci/_1/_1_4_PalindromePermutationTest.groovy
@@ -0,0 +1,26 @@
+package pap.lorinc.algos.ctci._1
+
+import spock.lang.Specification
+
+import static pap.lorinc.algos.ctci._1._1_4_PalindromePermutation.canBePalindrome
+
+class _1_4_PalindromePermutationTest extends Specification {
+    /*@formatter:off*/
+    def 'PalindromePermutation?'() {
+        expect: canBePalindrome(shuffle(string)) == verdict
+        where:  string                           || verdict
+                'Rats live on no evil star'      || true
+                'Eva, can I stab bats in a cave' || true
+                'Was it a car or a cat I saw?'   || true
+                "Dammit, I'm mad"                || true
+
+                'racecars'                       || false
+    }
+    /*@formatter:on*/
+
+    static shuffle(String string) {
+        def collect = string.collect()
+        Collections.shuffle(collect)
+        collect.join('')
+    }
+}
