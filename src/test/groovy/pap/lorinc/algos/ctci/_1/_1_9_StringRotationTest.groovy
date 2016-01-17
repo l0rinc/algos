5de1d5c387610498973f8a2e58a2f2ad34460f13
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-17 19:06:12 +0200

    CTCI 1.9: StringRotation

---
+++ src/test/groovy/pap/lorinc/algos/ctci/_1/_1_9_StringRotationTest.groovy
@@ -0,0 +1,21 @@
+package pap.lorinc.algos.ctci._1
+
+import spock.lang.Specification
+
+import static pap.lorinc.algos.ctci._1._1_9_StringRotation.isRotation
+
+class _1_9_StringRotationTest extends Specification {
+    /*@formatter:off*/
+    def 'rotate?'() {
+        expect: isRotation(source, target) == result
+        where:  source        | target        || result
+                ''            | ''            || true
+                'a'           | 'a'           || true
+                'waterbottle' | 'erbottlewat' || true
+                'apple'       | 'pleap'       || true
+
+                'ab'          | 'aba'         || false
+                'camera'      | 'macera'      || false
+    }
+    /*@formatter:on*/
+}
