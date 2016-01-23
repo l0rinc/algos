811b9279ac873674811bc71f3f7f17759b6cb17d
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-23 18:53:35 +0200

    CTCI 2.7: Intersection

---
+++ src/main/groovy/pap/lorinc/algos/ctci/_2/_2_7_Intersection.groovy
@@ -0,0 +1,23 @@
+package pap.lorinc.algos.ctci._2
+
+import static java.lang.Math.abs
+
+/** Find the intersecting node of two linked lists */
+class _2_7_Intersection {
+    static intersection(Node head1, Node head2) {
+        (head1, head2) = alignEnds(head1, head2)
+        while (head1.hasNext())
+            if (head1.is(head2))
+                return head1
+            else
+                (head1, head2) = [head1.next, head2.next]
+
+        Node.SENTINEL
+    }
+    static alignEnds(Node head1, Node head2) {
+        def (size1, size2) = [head1.size(), head2.size()]
+        def index = abs(size1 - size2)
+        (size1 > size2) ? [head1[index], head2]
+                        : [head1, head2[index]]
+    }
+}
