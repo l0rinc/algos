cf18c346d3cfd1cafe1a4d0113c32eb92a3ef37b
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-26 00:13:29 +0200

    Added LinkedList based (from chapter 2) Queue and Stack implementations for CTCI chapter 3

---
+++ src/main/groovy/pap/lorinc/algos/ctci/_3/Queue.groovy
@@ -0,0 +1,21 @@
+package pap.lorinc.algos.ctci._3
+
+import groovy.transform.*
+import pap.lorinc.algos.ctci._2.Node
+
+@EqualsAndHashCode(includes = 'head') @ToString(includes = 'head', includePackage = false)
+class Queue<T> {
+    private Node<T> head, tail
+
+    void add(T value) {
+        if (empty) head = (tail = new Node(value))
+        else tail = tail.addAfter(value)
+    }
+    T peek() { head.value }
+    T remove() {
+        T value = head.value
+        head = head.next
+        value
+    }
+    boolean isEmpty() { !head?.isValid() }
+}
