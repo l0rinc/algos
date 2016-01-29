71c2b2215f41545fd30d15205393196a49ecfb8b
Pap Lőrinc <paplorinc@yahoo.com>
2016-01-29 16:33:39 +0200

    CTCI 3.4: QueueViaStacks

---
+++ src/main/groovy/pap/lorinc/algos/ctci/_3/_3_4_QueueViaStacks.groovy
@@ -0,0 +1,25 @@
+package pap.lorinc.algos.ctci._3
+
+import groovy.transform.ToString
+
+/** Create a queue using two stacks */
+class _3_4_QueueViaStacks {
+    static createQueueViaStacks() { new QueueViaStacks() }
+}
+
+@ToString(includes = 'head', includePackage = false)
+class QueueViaStacks<T> {
+    private main = new Stack<T>()
+    private reverse = new Stack<T>()
+
+    void add(T value) { move(reverse, main).push(value) }
+    T peek() { move(main, reverse).peek() }
+    T remove() { move(main, reverse).pop() }
+    boolean isEmpty() { main.empty && reverse.empty }
+
+    private static move(Stack<T> from, Stack<T> to) {
+        while (!from.empty)
+            to.push(from.pop())
+        to
+    }
+}
