8ea6e504fb831a605d651378e05ec93b291fb9a4
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-05 01:28:14 +0200

    Further simplifications to DivisionToString

--- src/main/groovy/pap/lorinc/algos/sandbox/DivisionToString.groovy
+++ src/main/groovy/pap/lorinc/algos/sandbox/DivisionToString.groovy
@@ -1,8 +1,9 @@
 package pap.lorinc.algos.sandbox
 
-import groovy.transform.ToString
+import groovy.transform.*
 
 import static java.lang.Math.abs
+import static java.util.stream.Stream.iterate
 
 /* Given two integers, represent their ratio as (repeating) decimals */
 class DivisionToString {
@@ -10,42 +11,24 @@ class DivisionToString {
         if (divisor == 0) return null
 
         def isNegative = (dividend < 0) ^ (divisor < 0)
-        def (Map history, LinkedList results, int distance) = [[(0): -1], [], 1]
-        for (step in getSteps(abs(dividend), abs(divisor))) {
+        def (history, results, distance) = [[(0): -1], [] as LinkedList, 1]
+        for (step in iterate(Step.of(dividend, divisor), { Step.of(it.next, divisor) })) {
             results += step.ratio
 
-            def previousStep = history[step.next]
-            if (previousStep == null) history[step.next] = distance++
-            else return toString(results, previousStep, isNegative)
+            if (history[step.next] == null) history[step.next] = distance++
+            else return toString(results, history[step.next], isNegative)
         }
     }
-    static getSteps(int dividend, int divisor) {
-        new Iterator<DivisionStep>() {
-            def current = new DivisionStep(dividend, divisor)
-            @Override boolean hasNext() { true }
-            @Override DivisionStep next() {
-                def result = this.current
-                this.current = new DivisionStep(result.next, divisor)
-                result
-            }
-        }
-    }
-    static toString(LinkedList<Object> results, int repetitionStart, boolean negative) {
-        if (repetitionStart >= 0) {
-            results.add(repetitionStart, '(')
-            results.addLast(')')
-        }
+    static toString(LinkedList<Object> results, int repetitionStart, boolean isNegative) {
+        if (repetitionStart > 0) { results.add(repetitionStart, '('); results.addLast(')') }
         if (results.size() > 1) results.add(1, '.')
-        if (negative) results.addFirst('-')
+        if (isNegative) results.addFirst('-')
         results.join()
     }
 }
 
-@ToString class DivisionStep {
+@TupleConstructor @ToString class Step {
     int ratio, next
-
-    DivisionStep(int dividend, int divisor) {
-        this.ratio = dividend / divisor
-        this.next = (dividend % divisor) * 10
-    }
+    static of(int dividend, int divisor) { new Step(abs(dividend / divisor as int),
+                                                    abs(dividend % divisor) * 10) }
 }
