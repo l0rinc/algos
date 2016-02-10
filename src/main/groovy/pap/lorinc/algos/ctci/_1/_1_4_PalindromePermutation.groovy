d82b5ace446fd7df99c664b85deac4fb0eefa62f
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-11 00:00:02 +0200

    Added BitSet based solution to CTCI 1.4

--- src/main/groovy/pap/lorinc/algos/ctci/_1/_1_4_PalindromePermutation.groovy
+++ src/main/groovy/pap/lorinc/algos/ctci/_1/_1_4_PalindromePermutation.groovy
@@ -1,14 +1,31 @@
 package pap.lorinc.algos.ctci._1
 
+import static java.lang.Character.isLetter
 import static pap.lorinc.algos.helpers.Numbers.isEven
 
 /** Given a string, check whether palindromes can be formed from it */
 class _1_4_PalindromePermutation {
-    /** Complexity: O(string.size()), uses same amount of space */
     static canBePalindrome(String string) {
+        def linearLinear = canBePalindrome_Linear_Linear(string)
+        def linear = canBePalindrome_Linear(string)
+
+        assert linearLinear == linear
+        linear
+    }
+
+    /** Complexity: O(string.size()), uses same amount of space */
+    static canBePalindrome_Linear_Linear(String string) {
         def normalizedString = string.toLowerCase().replaceAll('\\W', '')
         def groups = normalizedString.toList().groupBy()
         def oddGroups = groups.findAll { k, v -> !isEven(v.size()) }
         oddGroups.size() <= 1
+    }
+
+    /** Complexity: O(string.size()), uses constant space */
+    static canBePalindrome_Linear(String string) {
+        def groups = new BitSet()
+        for (int c : string.toLowerCase())
+            isLetter(c) && groups.flip(c)
+        groups.cardinality() < 2;
     }
 }
