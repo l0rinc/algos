fa4d133b47ba260c0d8873c938e212bb4e446ddb
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-11 19:03:50 +0200

    Inlined the impl of CTCI 4.1

--- src/main/groovy/pap/lorinc/algos/ctci/_4/_4_1_RouteFind.groovy
+++ src/main/groovy/pap/lorinc/algos/ctci/_4/_4_1_RouteFind.groovy
@@ -1,11 +1,18 @@
 package pap.lorinc.algos.ctci._4
 
-import pap.lorinc.algos.ctci._4.utils.*
+import pap.lorinc.algos.ctci._4.utils.DiGraph
 
 /** Find out whether there's a route between two nodes in a digraph */
-class _4_1_RouteFind<T> {
+class _4_1_RouteFind {
     /** Complexity: O(graph.edgeCount()) */
     static isConnected(DiGraph graph, int v1, int v2) {
-        v2 == BreadthFirstSearch.of(graph, v1) { it != v2 }
+        def (Set marked, Queue queue) = [[v1], [v1] as ArrayDeque]
+        while (!queue.empty) {
+            def node = queue.remove()
+            if (node == v2) return true
+            marked[node] || graph.neighbours(node).each { queue.add(it) }
+            marked += node
+        }
+        false
     }
 }
