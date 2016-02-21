b7370b587b34c2fab43763c07a17a26cb1495981
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-21 15:55:49 +0200

    Changed 4_12 from squared to linear

--- src/main/groovy/pap/lorinc/algos/ctci/_04/_04_12_PathsWithSum.groovy
+++ src/main/groovy/pap/lorinc/algos/ctci/_04/_04_12_PathsWithSum.groovy
@@ -2,29 +2,24 @@ package pap.lorinc.algos.ctci._04
 
 import pap.lorinc.algos.ctci._04.utils.BiNode
 
-/** Return all paths in a binary tree that sum to a given value */
+import static pap.lorinc.algos.ctci._04.utils.BiNode.VisitorType.*
+
+/** Return the number of paths in a binary tree that sum to a given value */
 class _04_12_PathsWithSum {
-    /** Complexity: O(root.size**2) */
-    static sumPaths(BiNode root, int sum) {
-        Set results = []
-        iterate(root) { BiNode currentRoot ->
-            def (currentPath, currentSum) = [[], 0]
-            iterate(currentRoot, { BiNode<Integer> node -> // TODO some values should be precalculated (min/max?)
-                currentPath += node.value
+    /** Complexity: O(root.size), uses O(root.maxHeight) space */
+    static sumPathCount(BiNode root, int sum) {
+        def (cumulativeSums, currentSum, count) = [[:].withDefault { 0 }, 0, 0]
+        root.each(
+            (preOrder): { BiNode<Integer> node ->
+                cumulativeSums[currentSum]++
                 currentSum += node.value
-                if (currentSum == sum)
-                    results << currentPath.clone()
-            }, { currentSum -= currentPath.pop() })
-        }
-        results
-    }
-
-    private static iterate(BiNode node, Closure preOrder, Closure postOrder = {}) {
-        if (!node) return
-
-        preOrder(node)
-        iterate(node.left, preOrder, postOrder)
-        iterate(node.right, preOrder, postOrder)
-        postOrder(node)
+                count += cumulativeSums[currentSum - sum]
+            },
+            (postOrder): { BiNode<Integer> node ->
+                currentSum -= node.value
+                cumulativeSums[currentSum]-- // TODO Groovy should remove values that are equal to the default
+            }
+        )
+        count
     }
 }
--- src/test/groovy/pap/lorinc/algos/ctci/_04/_04_12_PathsWithSumTest.groovy
+++ src/test/groovy/pap/lorinc/algos/ctci/_04/_04_12_PathsWithSumTest.groovy
@@ -1,26 +1,29 @@
 package pap.lorinc.algos.ctci._04
 
+import pap.lorinc.algos.ctci._04.utils.BiNode as N
 import pap.lorinc.algos.ctci._04.utils.BiNodeBuilder
 import spock.lang.*
 
-import static pap.lorinc.algos.ctci._04._04_12_PathsWithSum.sumPaths
+import static pap.lorinc.algos.ctci._04._04_12_PathsWithSum.sumPathCount
 
 @Unroll class _04_12_PathsWithSumTest extends Specification {
     /*@formatter:off*/
-    def 'sumList from #tree?'() {
+    def 'sumPathCount from #tree?'() {
         when:   def root = BiNodeBuilder.from(tree) 
-        then:   expectedPaths.each { sum, expected -> 
-                    def actual = sumPaths(root, sum)
-                    assert (actual - [[sum]]) == expected.toSet() 
-                }
+        then:   expectedPathCounts.each { sum, expected -> assert sumPathCount(root, sum) == expected }
         
         where:
-        tree                                                       | expectedPaths
-        [1:[0]]                                                    | [1:[[1,0]]]
-        [1:[0,2]]                                                  | [1:[[1,0]]]
-        [2:[1,3],1:-1]                                             | [2:[[2,1,-1]],3:[[2,1]]]
-        [3:[2,4],2:0,4:-5]                                         | [2:[[3,4,-5],[2,0]],5:[[3,2],[3,2,0]]]
-        [12:[1,13],1:[0,6],6:[3,9],3:[2,5],9:[8,10],5:4,8:7,10:11] | [1:[[1,0]],5:[[3,2]],7:[[1,6]],8:[[3,5]],9:[[6,3],[5,4]],10:[[1,6,3]],11:[[6,3,2]],12:[[1,6,3,2],[3,5,4]],13:[[12,1],[12,1,0]]] 
+        tree                                                       | expectedPathCounts
+        [1:[0]]                                                    | [0:1,1:2,3:0]
+        [1:[0,2]]                                                  | [1:2,2:1,3:1]
+        [2:[1,3],1:-1]                                             | [2:2,3:2]
+        [3:[2,4],2:0,4:-5]                                         | [2:3,5:2]
+        [5:[3,1],3:[-8,8],1:[2,6],2:0]                             | [0:2] 
+        [12:[1,13],1:[0,6],6:[3,9],3:[2,5],9:[8,10],5:4,8:7,10:11] | [1:2,3:1,5:2,7:2,8:2,9:3,10:2,11:2,12:3,13:3] 
+
+        new N(-6,new N(1,new N(4,new N(1,new N(5,new N(0,new N(-1)))))))                                                 | [5:7] 
+        new N(0,new N(0),new N(0,new N(0,null,new N(0)),new N(0)))                                                       | [0:15, 4:0] 
+        new N(-7,new N(-7,null,new N(1,new N(2))),new N(7,new N(3),new N(20,new N(0,new N(-3,null,new N(2,new N(1))))))) | [0:4] 
     }
     /*@formatter:on*/
 }
