1def0ac84e09cb5ddee3f13638c2b292b8caa142
Pap Lőrinc <paplorinc@yahoo.com>
2016-02-25 23:05:09 +0200

    CTCI 5.07: PairwiseSwap

---
+++ src/test/groovy/pap/lorinc/algos/ctci/_05/_05_07_PairwiseSwapTest.groovy
@@ -0,0 +1,32 @@
+package pap.lorinc.algos.ctci._05
+
+import spock.lang.*
+
+import static pap.lorinc.algos.ctci._05._05_07_PairwiseSwap.swapOddEvenBits
+
+@Unroll class _05_07_PairwiseSwapTest extends Specification {
+    /*@formatter:off*/
+    def 'swap #source == #target?'() {
+        expect: swapOddEvenBits(source) == target 
+        where:  source || target  
+                0b0000 || 0b0000  
+                0b0001 || 0b0010  
+                0b0010 || 0b0001  
+                0b0011 || 0b0011  
+                0b0100 || 0b1000  
+                0b0101 || 0b1010  
+                0b0110 || 0b1001  
+                0b0111 || 0b1011  
+                0b1000 || 0b0100  
+                0b1001 || 0b0110  
+                0b1010 || 0b0101  
+                0b1011 || 0b0111  
+                0b1100 || 0b1100  
+                0b1101 || 0b1110  
+                0b1110 || 0b1101  
+                0b1111 || 0b1111  
+                
+                0b1001010101001010 || 0b0110101010000101
+    }
+    /*@formatter:on*/
+}
