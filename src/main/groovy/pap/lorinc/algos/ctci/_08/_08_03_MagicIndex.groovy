19902e0afb9d25377b0fcbcbf4087171309ea182
Pap Lőrinc <paplorinc@yahoo.com>
2016-07-19 15:26:36 +0300

    Fixed MagicIndex to work with duplicates also

--- src/main/groovy/pap/lorinc/algos/ctci/_08/_08_03_MagicIndex.groovy
+++ src/main/groovy/pap/lorinc/algos/ctci/_08/_08_03_MagicIndex.groovy
@@ -1,15 +1,21 @@
 package pap.lorinc.algos.ctci._08
 
-/** Find the index that equals the value in a sorted list */
+/** Find the index that equals the value in an ascending (unique?) list */
 class _08_03_MagicIndex {
     /** Complexity: O(log elements.length) */
     static magicIndex(List<Integer> elements, int startInclusive = 0, int endExclusive = elements.size()) {
-        if (startInclusive >= endExclusive) return null
+        if (startInclusive > endExclusive) return null
 
         int middleIndex = startInclusive + ((endExclusive - startInclusive) / 2)
         def middleElement = elements[middleIndex]
-        if      (middleIndex < middleElement) return magicIndex(elements, startInclusive, middleIndex)
-        else if (middleIndex > middleElement) return magicIndex(elements, middleIndex + 1, endExclusive)
-        else                                  return middleIndex
+
+        if (middleIndex == middleElement)
+            return middleIndex
+        else {
+            def index = magicIndex(elements, startInclusive, [middleIndex - 1, middleElement].min())
+            if (index == null)
+                index = magicIndex(elements, [middleIndex + 1, middleElement].max(), endExclusive)
+            return index
+        }
     }
 }
