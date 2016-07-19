e09c3ac5d8cb17a60be33ecfc9efe2ac40b453cf
Pap Lőrinc <paplorinc@yahoo.com>
2016-07-19 21:39:01 +0300

    Added parameter sorting to RecursiveMultiply

--- src/main/groovy/pap/lorinc/algos/ctci/_08/_08_05_RecursiveMultiply.groovy
+++ src/main/groovy/pap/lorinc/algos/ctci/_08/_08_05_RecursiveMultiply.groovy
@@ -1,16 +1,19 @@
 package pap.lorinc.algos.ctci._08
 
+import groovy.transform.Memoized
+
 import static pap.lorinc.algos.helpers.Numbers.isOdd
 
 /** */
 class _08_05_RecursiveMultiply {
-    /** Complexity: O(log multiplier) */
+    /** Complexity: O(log min(multiplier, multiplicand)) */
     static multiply(int multiplier, int multiplicand) {
-        if (multiplier == 1)
-            multiplicand
+        def (int small, int big) = [multiplier, multiplicand].sort()
+        if (small == 1)
+            big
         else {
-            def oddFactor = isOdd(multiplier) ? multiplicand : 0
-            oddFactor + multiply(multiplier >> 1, multiplicand << 1)
+            def oddFactor = isOdd(small) ? big : 0
+            oddFactor + multiply(small >> 1, big << 1)
         }
     }
 }
